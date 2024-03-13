204ad44a168954ffc6022d9effd95c7f7f898d26
Muqtadiryullah <160833567+Muqtadiryullah@users.noreply.github.com>
2024-03-13 10:12:39 +0800

    Add files via upload

---
+++ Tugas 3 PBO/main2.java
@@ -0,0 +1,15 @@
+/*
+Nama : M. Muqtadiryullah Iskandar
+NIM : 13020220112
+Hari/Tgl : Selasa, 12 Maret 2024
+Jam : 23.45
+ */
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true)
+        { System.out.print ("Print satu baris ....\n");
+        }
+
+    }
+}
